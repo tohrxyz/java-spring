36c2f2bed2e8c5732f680e44e43d6bcda14ba3a4
tomashrib <tomas.hrib@proton.me>
2023-03-23 08:28:50 +0100

    Initial commit for spring-web

---
+++ spring-web/spring_edu/src/main/java/xyz/tohrxyz/java/spring/spring_edu/SpringEduApplication.java
@@ -0,0 +1,13 @@
+package xyz.tohrxyz.java.spring.spring_edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEduApplication.class, args);
+	}
+
+}
---
+++ spring-web/spring_edu/src/test/java/xyz/tohrxyz/java/spring/spring_edu/SpringEduApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tohrxyz.java.spring.spring_edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
