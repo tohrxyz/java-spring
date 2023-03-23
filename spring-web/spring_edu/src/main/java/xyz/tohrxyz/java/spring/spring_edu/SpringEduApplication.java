5247e828ab43b3137021c6c71d94593208ab9597
tomashrib <tomas.hrib@proton.me>
2023-03-23 09:08:59 +0100

    feat: display hello world message on website

--- spring-web/spring_edu/src/main/java/xyz/tohrxyz/java/spring/spring_edu/SpringEduApplication.java
+++ spring-web/spring_edu/src/main/java/xyz/tohrxyz/java/spring/spring_edu/SpringEduApplication.java
@@ -2,8 +2,10 @@ package xyz.tohrxyz.java.spring.spring_edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"xyz.tohrxyz.java.spring.spring_edu"})
 public class SpringEduApplication {
 
 	public static void main(String[] args) {
